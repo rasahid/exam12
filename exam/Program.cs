920919d64c65dddb98a5979d2fcf80c7756cf4d3
Rashid Rasooli <65131865+rasahid@users.noreply.github.com>
2023-12-11 20:31:58 -0500

    Update Program.cs

--- exam/Program.cs
+++ exam/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class ValueContainer<T> where T : struct
+{
+    private List<T> values = new List<T>();
+
+    public void AddItem(T item)
+    {
+        values.Add(item);
+    }
+
+    public T GetItem(int index)
+    {
+        if (index >= 0 && index < values.Count)
+        {
+            return values[index];
+        }
+        else
+        {
+            throw new IndexOutOfRangeException("Index is out of range.");
+        }
+    }
+
+    public List<T> GetSortedDescending()
+    {
+        return values.OrderByDescending(x => x).ToList();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ValueContainer<int> intContainer = new ValueContainer<int>();
+
+        intContainer.AddItem(10);
+        intContainer.AddItem(1);
+        intContainer.AddItem(75);
+
+        Console.WriteLine("Original Collection:");
+        foreach (var item in intContainer.GetSortedDescending())
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
